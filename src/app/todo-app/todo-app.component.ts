c120211d038a684aeb52c6f971fe5c969f70337e
shankerk <shankerk.in@mouritech.com>
2024-02-01 22:05:43 +0530

    validation and soft delete functionality

--- src/app/todo-app/todo-app.component.ts
+++ src/app/todo-app/todo-app.component.ts
@@ -1,25 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Todo } from './../todointerface';
 import { Component, inject, Input } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TodoServiceService } from '../todo-service.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 export class NgbdPaginationBasic {
-  page:number = 1;
+  page: number = 1;
 }
 @Component({
   selector: 'app-todo-app',
   standalone: true,
   imports: [CommonModule, FormsModule, ReactiveFormsModule,
-    HttpClientModule,NgbPaginationModule],
+    HttpClientModule, NgbPaginationModule],
   templateUrl: './todo-app.component.html',
   styleUrl: './todo-app.component.css'
 })
 export class TodoAppComponent {
-  pagination: NgbdPaginationBasic = new NgbdPaginationBasic();
+  TodoFormData!: FormGroup
   TodoList: Todo[] = []
   editId: any
   editTodoName: string = ''
@@ -32,11 +32,19 @@ export class TodoAppComponent {
   constructor(private TodoListData: TodoServiceService) {
   }
   ngOnInit(): void {
+    this.TodoFormData = new FormGroup({
+      todoName: new FormControl('', [Validators.required, Validators.maxLength(15)]),
+      tododescription: new FormControl('', [Validators.required]),
+      todoisDone: new FormControl('', [Validators.required])
+    }
+    )
     this.reloadTodoList();
   }
   updateMode(Todo: any) {
     this.editId = Todo.id
     this.editTodoName = Todo.TodoName
+    this.editdescription = Todo.description
+    this.editisDone = Todo.isDone
     this.editMode = !this.editMode;
   }
   discart(tododata: Todo, objid: number) {
@@ -44,8 +52,8 @@ export class TodoAppComponent {
     this.TodoList[obj] = tododata
     this.editId = ''
     this.editTodoName = ''
-    this.editdescription=''
-    this.editisDone=false
+    this.editdescription = ''
+    this.editisDone = false
     this.editMode = !this.editMode;
   }
 
@@ -54,7 +62,7 @@ export class TodoAppComponent {
       this.TodoList = data;
     });
   }
-  onSubmit(todoName: string, tododescription: string,todoisDone: boolean) {
+  onSubmit(todoName: string, tododescription: string, todoisDone: boolean) {
     this.todoName = todoName;
     this.tododescription = tododescription;
     this.todoisDone = todoisDone;
@@ -66,29 +74,35 @@ export class TodoAppComponent {
       this.TodoList.push(response);
     });
     this.todoName = '';
-    this.tododescription ='';
-    this.todoisDone =false;
+    this.tododescription = '';
+    this.todoisDone = false;
   }
 
   update(data: any) {
-    this.TodoListData.updateTodo(data).subscribe(response => {
-      // this.TodoList.filter(todo=>{
-      //   todo.id===this.editId
-      //   console.log()
-      //   })
-      let obj = this.TodoList.findIndex(todo => todo.id === this.editId)
-      this.TodoList[obj] = data
-      this.editId = ''
-      this.editTodoName = ''
+    let obj = this.TodoList.find(todo => todo.id === data.id)
+    console.log(obj)
+    if (data.isDone) {
       this.editMode = !this.editMode;
-    })
+    }
+    else {
+      this.TodoListData.updateTodo(data).subscribe(response => {
+        let obj = this.TodoList.findIndex(todo => todo.id === this.editId)
+        this.TodoList[obj] = data
+        this.editId = ''
+        this.editTodoName = ''
+        this.editdescription = ''
+        this.editisDone = false
+        this.editMode = !this.editMode;
+      })
+    }
   }
   delete(Todo: any): void {
     let todoId: any = this.TodoList.findIndex(todolist => todolist.id === Todo.id);
     if (todoId !== -1) {
       this.TodoListData.deleteTodo(Todo.id).subscribe((res: any) => {
         console.log(todoId);
-        this.TodoList.splice(todoId, 1);
+        // this.TodoList.splice(todoId, 1);
+        this.TodoList[todoId]['isDone'] = true
       })
     }
   }
